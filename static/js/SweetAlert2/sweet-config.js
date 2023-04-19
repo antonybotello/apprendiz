014a82084f1872e166e8fc6755e941e6c0f519cd
Antony Botello <antonybotello@gmail.com>
2023-04-19 13:34:07 -0500

    fondo sweet alert

--- static/js/SweetAlert2/sweet-config.js
+++
@@ -1,9 +0,0 @@
-Swal.fire({
-  position: 'top-end',
-  icon: 'success',
-  title: 'Your work has been saved',
-  toast: true,
-  showConfirmButton: false,
-  timer: 1500,
-  timerProgressBar: true
-})
