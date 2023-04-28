9ca2b77803872b40259bb141ab6897cc634e9ae6
Antony Botello <antonybotello@gmail.com>
2023-04-28 07:36:49 -0500

    pillow

--- base/settings.py
+++ base/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',# pip install django-bootstrap5
     'crispy_forms', # pip install django-crispy-forms
     'crispy_bootstrap5', # pip install crispy-bootstrap5
+     # pip install pillow
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS= "bootstrap5"
 CRISPY_TEMPLATE_PACK= "bootstrap5"
